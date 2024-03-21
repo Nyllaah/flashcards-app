0efb7a029248866eb526da4b818f71b40f5c015f
Nyllaah <nylla.g@hotmail.com>
2024-03-21 19:39:56 -0300

    feat: answer component

---
+++ frontend/src/components/Answer.tsx
@@ -0,0 +1,17 @@
+function Answer (props: {answer: string}) {
+  return (
+    <>
+      <div>
+        <p>{props.answer}</p>
+      </div>
+      <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+      </div>
+    </>
+)}
+
+export default Answer;
