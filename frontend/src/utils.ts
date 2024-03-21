bd87dbade5b21fa8247191f157a37cf58a38997b
Nyllaah <nylla.g@hotmail.com>
2024-03-21 20:45:36 -0300

    fix: no repeted dis

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -3,7 +3,10 @@ const getRandomInt = (max: number): number => Math.floor(Math.random() * max);
 export const getRandomIds = (max: number): number[] => {
   const ids: number[] = [];
   for (let n = 1; n <= 15; n++) {
-    ids.push(getRandomInt(max));
+    const int = getRandomInt(max);
+    if (!ids.some((id) => id === int)) {
+      ids.push(getRandomInt(max));
+    }
   }
   return ids;
 };
