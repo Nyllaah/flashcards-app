1d9deb249d5a338a0f9506672813e5e5614ebd64
Nyllaah <nylla.g@hotmail.com>
2024-04-11 20:56:28 -0300

    refact: answer container undone

--- frontend/src/components/Flashcard.tsx
+++ frontend/src/components/Flashcard.tsx
@@ -1,20 +1,28 @@
 import { useSelector, useDispatch  } from 'react-redux';
-import { endLesson, showAnswer } from "../redux/actions";
+import { endLesson, nextFlashcard, showAnswer, updateStatus } from "../redux/actions";
 
-import Answer from './Answer';
 import EndOfLesson from './EndOfLesson';
 import Button from './Button';
 
-import { FlashcardType, StateType } from '../../types';
+import { FlashcardType, StateType, StatusType } from '../../types';
 
 import styles from './css/Flashcard.module.css';
 import WordContainer from './WordContainer';
 
 function Flashcard() {
-  const { visibleAnswer, cardIndex, currLessonCards } = useSelector((state: StateType) => state.flashcardReducer);
+  const { allFlashcards, visibleAnswer, cardIndex, currLessonCards } = useSelector((state: StateType) => state.flashcardReducer);
   const currCard:FlashcardType = currLessonCards[cardIndex];
   
   const dispatch = useDispatch();
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>, id: number) => {
+    const { innerHTML } = e.target as HTMLButtonElement;
+    const selectedStatus = +innerHTML as StatusType;
+  
+    const updated = allFlashcards.map((card: FlashcardType) => card.id === id ? {...card, status: selectedStatus} : card);
+    dispatch(updateStatus(updated))
+    dispatch(nextFlashcard())
+  };
 
   return (
     <div className={ styles.flashcard }>
@@ -32,12 +40,28 @@ function Flashcard() {
           <WordContainer
             content={ currCard.question }
             type={'question'}
-            visibleAnswer
+            visibleAnswer={ visibleAnswer }
           />
 
           {!visibleAnswer 
           ? <Button content={'See Answer'} action={ () => dispatch(showAnswer()) }/>
-          : <Answer answer={ currCard.answer } id={ currCard.id } />}
+          : <>
+              <WordContainer
+                content={currCard.answer}
+                type={'answer'}
+                visibleAnswer={ visibleAnswer }
+              />
+    
+              <div className={styles.statusBtnContainer}>
+                {[1, 2, 3, 4, 5].map((status) => (
+                  <button
+                  className={ styles.statusBtn }
+                  style={{backgroundColor: `var(--status-${status})`}}
+                  onClick={ (e) => handleClick(e, currCard.id) }>{status}</button>
+                ))}
+              </div>
+            </>
+          }
           
         </section>
         <footer className={ styles.footer }>
