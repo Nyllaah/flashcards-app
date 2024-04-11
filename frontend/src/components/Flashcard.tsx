e6885fcea3111056fb80b9c9203a0e62b1e24700
Nyllaah <nylla.g@hotmail.com>
2024-04-11 17:03:32 -0300

    refatc: flashcard styling

--- frontend/src/components/Flashcard.tsx
+++ frontend/src/components/Flashcard.tsx
@@ -1,34 +1,20 @@
 import { useSelector, useDispatch  } from 'react-redux';
-import { showAnswer } from "../redux/actions";
+import { endLesson, showAnswer } from "../redux/actions";
 
 import Answer from './Answer';
 import EndOfLesson from './EndOfLesson';
+import Button from './Button';
 
 import { StateType } from '../redux/reducers/flashcardReducer';
 import { FlashcardType } from '../../../db/types';
 
 import styles from './css/Flashcard.module.css';
-import Button from './Button';
 
 function Flashcard() {
   const { visibleAnswer, cardIndex, currLessonCards } = useSelector((state: StateType) => state.flashcardReducer);
   const currCard:FlashcardType = currLessonCards[cardIndex];
+  
   const dispatch = useDispatch();
-
-  const getStatusColor = (status: number) => {
-    switch (status) {
-      case 1:
-        return 'var(--status-1)';
-      case 2:
-        return 'var(--status-2)';
-      case 3:
-        return 'var(--status-3)';
-      case 4:
-        return 'var(--status-4)';
-      case 5:
-        return 'var(--status-5)';
-    }
-  }
 
   return (
     <div className={styles.flashcard}>
@@ -37,22 +23,24 @@ function Flashcard() {
       : <>
         <header
           className={styles.header}
-          style={{backgroundColor: getStatusColor(currCard.status)}}
+          style={{backgroundColor: `var(--status-${currCard.status})`}}
         >
           <span>{cardIndex + 1}</span>
         </header>
 
-        <section>
-          <div className={styles.question} style={visibleAnswer ? {height: 'calc(var(--height) / 2)'}: {}}>
-            <p>{currLessonCards[cardIndex].question}</p>
+        <section className={styles.container}>
+          <div className={styles.question} onClick={() => dispatch(showAnswer())} style={visibleAnswer ? {height: 'calc(var(--height) / 2)'}: {}}>
+            <span>{currCard.question}</span>
           </div>
 
           {!visibleAnswer 
           ? <Button content={'See Answer'} action={() => dispatch(showAnswer())}/>
-          : <Answer answer={currLessonCards[cardIndex].answer} id={currLessonCards[cardIndex].id} />}
+          : <Answer answer={currCard.answer} id={currCard.id} />}
           
-          <button>End Lesson</button>
         </section>
+        <footer className={styles.footer}>
+          <Button content={'End Lesson'} action={() => dispatch(endLesson())} />
+        </footer>
       </>
       }
     </div>
