805de64032acf635b01698a93eea517f4d4238e5
Nyllaah <nylla.g@hotmail.com>
2024-04-11 16:59:06 -0300

    fix: button fix margin and action

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,7 +2,7 @@ import styles from './css/Button.module.css';
 
 function Button (props: {content: string, action: () => any}) {
   return (
-    <button className={styles.button} onClick={props.action()}>{props.content}</button>
+    <button className={ styles.button } onClick={ props.action }>{props.content}</button>
   )
 }
 
