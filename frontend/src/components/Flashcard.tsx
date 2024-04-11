bafebf65bd3942825d133648d18000f8a4e9a093
Nyllaah <nylla.g@hotmail.com>
2024-04-11 12:24:26 -0300

    feat: flashcard css

--- frontend/src/components/Flashcard.tsx
+++ frontend/src/components/Flashcard.tsx
@@ -6,21 +6,47 @@ import EndOfLesson from './EndOfLesson';
 
 import { StateType } from '../redux/reducers/flashcardReducer';
 
+import styles from './css/Flashcard.module.css';
+import { FlashcardType } from '../../../db/types';
+
 function Flashcard() {
   const { visibleAnswer, cardIndex, currLessonCards } = useSelector((state: StateType) => state.flashcardReducer);
+  const currCard:FlashcardType = currLessonCards[cardIndex];
+
+  const getStatusColor = (status: number) => {
+    switch (status) {
+      case 1:
+        return 'var(--status-1)';
+      case 2:
+        return 'var(--status-2)';
+      case 3:
+        return 'var(--status-3)';
+      case 4:
+        return 'var(--status-4)';
+      case 5:
+        return 'var(--status-5)';
+    }
+  }
 
   return (
-    <>
+    <div className={styles.flashcard}>
       {cardIndex === currLessonCards.length
       ? <EndOfLesson />
       : <div>
-        <p>{`Current Status: ${currLessonCards[cardIndex].status}`}</p>
+        <header
+          className={styles.header}
+          style={{backgroundColor: getStatusColor(currCard.status)}}
+        >
+          <span>{cardIndex + 1}</span>
+        </header>
+        <section>
           <Question question={currLessonCards[cardIndex].question}/>
           {visibleAnswer && <Answer answer={currLessonCards[cardIndex].answer} id={currLessonCards[cardIndex].id} />}
           <button>End Lesson</button>
+        </section>
       </div>
       }
-    </>
+    </div>
   )
 }
 
