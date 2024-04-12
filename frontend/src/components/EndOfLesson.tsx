8411f66d8ac5f1e57ad8149c893851330a9d98a7
Nyllaah <nylla.g@hotmail.com>
2024-04-12 14:23:03 -0300

    feat: ned of lesson css

--- frontend/src/components/EndOfLesson.tsx
+++ frontend/src/components/EndOfLesson.tsx
@@ -1,14 +1,16 @@
 import { useDispatch } from "react-redux";
 import { endLesson } from "../redux/actions";
+import styles from './css/EndOfLesson.module.css';
+import Button from "./Button";
 
 function EndOfLesson () {
   const dispatch = useDispatch();
 
   return (
-    <>
-      <p>Congratulations!</p>
-      <button onClick={() => dispatch(endLesson())}>Go Back</button>
-    </>
+    <div className={ styles.container }>
+      <h2 className={ styles.title } >Congratulations!</h2>
+      <Button content={'Go Back'} action={() => dispatch(endLesson())} />
+    </div>
   )
 }
 
